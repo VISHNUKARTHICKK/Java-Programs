d52a2d51af5e630ffcae96f39a804581c98145d8
VISHNUKARTHICKK <vkarthick459@gmail.com>
2025-09-20 22:08:31 +0530

    number commit

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class ReverseString {
 
     public static void main(String[] args) {
-        String str="method";
+       Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
         String rev="";
         for(int i=str.length()-1;i>=0;i--){
             rev=rev+str.charAt(i);
