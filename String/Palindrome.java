a79bb1a6daee1c3111aa14623d034dc3775afe72
VISHNUKARTHICKK <vkarthick459@gmail.com>
2025-09-20 22:04:21 +0530

    string commit

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Palindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("palindrome");
+        }else{
+            System.out.println("not a palindrome");
+        }
+    }
 }
