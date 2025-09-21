d746ab7cbe6bf902656de8d40cb165c2e9d52549
VISHNUKARTHICKK <vkarthick459@gmail.com>
2025-09-21 22:15:48 +0530

    number commit

---
+++ Numbers/ReverseNum.java
@@ -0,0 +1,12 @@
+public class ReverseNum {
+public static void main(String[] args) {
+    int num=12345;
+    int rev=0;
+    while(num!=0){
+        int temp=num%10;
+        rev=rev*10+temp;
+        num=num/10;
+    }
+    System.out.println("Reversed Number: "+rev);
+}
+}
