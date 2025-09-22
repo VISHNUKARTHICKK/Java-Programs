e1ae1af3a849cddfd11a6a9d73ba364dff4a0ca0
VISHNUKARTHICKK <vkarthick459@gmail.com>
2025-09-22 11:12:43 +0530

    array commit

---
+++ Array/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+  public static void main(String[] args) {
+    int [] arr={2,5,8,4,1,6};
+    int no=4;
+      for (int i = 0; i < arr.length; i++) {
+        if (arr[i]==no) {
+            System.out.println("Element is present in the index of: "+i);
+        }
+      }
+  }
+}
