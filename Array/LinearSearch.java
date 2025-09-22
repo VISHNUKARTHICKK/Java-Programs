7d838274cff3ab27d3fc0e520a317eb5f22e37da
VISHNUKARTHICKK <vkarthick459@gmail.com>
2025-09-22 11:16:23 +0530

    array elements

--- Array/LinearSearch.java
+++ Array/LinearSearch.java
@@ -1,11 +1,18 @@
 public class LinearSearch {
   public static void main(String[] args) {
     int [] arr={2,5,8,4,1,6};
-    int no=4;
+    int no=8;
+    boolean flag=false;
       for (int i = 0; i < arr.length; i++) {
         if (arr[i]==no) {
             System.out.println("Element is present in the index of: "+i);
+            flag=true;
+            break;
         }
       }
-  }
+      if (flag==false) {
+        System.out.println("Element is not present in the array"); 
+        
+      } 
+     }
 }
